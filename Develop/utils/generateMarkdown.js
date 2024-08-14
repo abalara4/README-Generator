a394127b5370e39ef76a094e927809d45c0c3501
abalara4 <abalara4@gmail.com>
2024-08-13 21:26:30 -0400

    edits fixed

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -16,6 +16,32 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+function renderLicenseLink(license) {
+    if (license === 'MIT') {
+        return `[MIT](https://opensource.org/licenses/MIT)`
+    } else if (license === 'GNU GPLv3') {
+        return `[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0)`
+    } else if (license === 'Apache 2.0') {
+        return `[Apache 2.0](https://opensource.org/licenses/Apache-2.0)`
+    } else if (license === 'ISC') {
+        return `[ISC](https://opensource.org/licenses/ISC)`
+    } else {
+        return '';
+    }
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    if (license === 'None') {
+        return '';
+    } else {
+        return `## License
+        This project is licensed under the ${license} license.`
+    }
+}
+
+// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
